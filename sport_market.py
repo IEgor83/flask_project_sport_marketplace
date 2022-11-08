7f2a659145a9fb3a9b4d965fb08b9c2c6269728f
Egor <e.r.ivanov@inbox.ru>
2022-11-08 21:09:13 +0400

    add registration and authorization pages

--- sport_market.py
+++ sport_market.py
@@ -8,5 +8,15 @@ def main_page():
     return '''HELLO'''
 
 
+@app.route("/registration")
+def registration():
+    return '''Регистрация'''
+
+
+@app.route("/authorization")
+def authorization():
+    return '''Авторизация'''
+
+
 if __name__ == "__main__":
     app.run(debug=True)
